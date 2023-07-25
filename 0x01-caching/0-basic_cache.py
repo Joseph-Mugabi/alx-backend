5066bf7938b9fdb5d37642b88e69a7114157cefb
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-07-25 22:35:21 +0300

    basic cache for store and retrive data

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+implementing basic cache class that inherits from parent BaseCaching.
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    representing the class BaseCaching
+    """
+    def __init__(self):
+        # call the parent class constructor to initialize the cache_data dict
+        super().__init__()
+
+    def put(self, key, item):
+        #  check if key or item are not none,
+        #  and proceed to assign the item value to the key in cache_data dict
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        # check if key is None or if the key doesn't exist 
+        # in the cache_data dict
+        if key is None or key not in self.cache_data:
+            return None
+
+        #  return value associated with the key in the cache_data dict
+        return self.cache_data[key]
